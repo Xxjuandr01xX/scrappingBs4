0e11ade0b311e818e574c6acdfc44ca5ad668d32
Juan D. Rincon U <jd.rincon021@gmail.com>
2023-01-14 19:10:05 -0400

    actualizacion de acrspping

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+
 ## Realizar un web Scrapping con la libreria Beutifuk Soup
 ## sitio scrapeado paraScrapear.com
 
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://parascrapear.com/")
-soup = BeautifulSoup(page.text, "html.parser")
-buscar_bloque = soup.find_all("blockquote")
-for bloque in buscar_bloque:
-    author = bloque.find(class_= "author").text
-    categorias = bloque.find(class_= "cat").text
-    frase = bloque.find('q')
-    print([author, categorias, frase])
+if "__main__" == __name__:
+    page = requests.get("https://parascrapear.com/")
+    soup = BeautifulSoup(page.text, "html.parser")
+    buscar_bloque = soup.find_all("blockquote")
+    for bloque in buscar_bloque:
+        try:
+            author = bloque.find(class_= "author").text
+            categorias = bloque.find(class_= "cat").text
+            frase = bloque.find('q')
+
+            Texto_scrappeado = "{} \n -{} ({}) \n"
+
+            print(Texto_scrappeado.format(frase, author, categorias))
+        except:
+
+            print()
